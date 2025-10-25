c8cd0e8079ca92afdd60d32125c491a938741692
Abdelrahman099 <abdelrahman.osama.099@gmail.com>
2025-10-25 17:40:29 +0300

    Fix:OverFlow

--- dashboard/src/components/ui/card.tsx
+++ dashboard/src/components/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card"
       className={cn(
-        "bg-gradient-to-br border-border from-primary/10 to-secondary/10 text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
+        "bg-gradient-to-br border-border from-primary/10 to-secondary/10 text-card-foreground flex flex-col gap-6 rounded-xl border py-4 shadow-sm",
         className
       )}
       {...props}
